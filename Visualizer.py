99f55850a1aa0597818cd939257f79944eba43b5
Sami <iamsamimk@gmail.com>
2026-06-19 23:25:43 -0400

    Delete Visualizer.py

--- Visualizer.py
+++
@@ -1,6 +0,0 @@
-class Visualizer:
-    """The visualizer.
-
-    This class handles the frontend of the application and covers aspects such
-    as event handling, user interaction, and authentication.
-    """
